92c69ecd6f3f359e9669b335551b55723923e6cb
dds <damandeep.in@gmail.com>
2023-02-13 23:22:57 +0530

    Added example in home-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DatePickerComponent } from './components/dpkr/date-picker/date-picker.component';
 import { HomeComponent } from './components/home/home/home.component';
 
 const routes: Routes = [{path:'',component:HomeComponent}];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DatePickerComponent } from './components/dpkr/date-picker/date-picker.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeComponent } from './components/home/home/home.component';
-
+import {AngularCustomDatepickerModule} from 'angular-custom-datepicker'
 @NgModule({
   declarations: [
     AppComponent,
-    DatePickerComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
+    AngularCustomDatepickerModule,
   ],
   providers: [ { provide: Window, useValue: window }],
   bootstrap: [AppComponent]
